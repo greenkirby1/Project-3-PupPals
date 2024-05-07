bf815391821e87bf504e8b3a50eac4943cd07c58
Filom3na <filomenamurgo@yahoo.com>
2024-05-07 11:45:30 +0100

    pupSchema

---
+++ models/pup.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const pupSchema = new mongoose.Schema({
+  pupName: {
+    type: String,
+    required: true
+  },
+  image: String,
+  birthday: {
+    type: String,
+    required: true,
+  },
+  breed: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  neutered: {
+    type: Boolean,
+    default: false
+  },
+  favorites: [String],
+  dislikes: [String],
+  bones: { type: Number },
+  owner: {
+    type: mongoose.ObjectId,
+    ref: 'User',
+    required: true
+  }
+})
