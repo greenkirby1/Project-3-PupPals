f47fde675b32dbb1a99f16c073cdb7ed6bd37304
Cassie <cassie.lee1023@gmail.com>
2024-05-09 12:51:08 +0100

    saving progress on profile

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,9 +1,30 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
+
 export default function Profile() {
+
+  // * States
+  const [userProfile, setUserProfile] = useState()
+  const [error, setError] = useState('')
+
+  const { VITE_BASE_URL } = import.meta.env
+
+  console.log(VITE_BASE_URL)
 
   // API Calls
   // api/profile
   // api/chats 123
-  // api/chats/1 1
+  useEffect(() => {
+    async function getUserProfile() {
+      try {
+        const { data } = await axios.get(``)
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getUserData()
+  }, [userProfile])
 
   return <h1>Profile</h1>
 }
