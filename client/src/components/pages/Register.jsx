34db566278ac71315936c7de71cb3665d422f5aa
Molly Gregson <mollykg123@msn.com>
2024-05-09 16:25:19 +0100

    added Form Component

--- client/src/components/pages/Register.jsx
+++ client/src/components/pages/Register.jsx
@@ -1,3 +1,31 @@
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import Form from '../subcomponents/Form.jsx'
+
 export default function Register() {
-  return <h1>Register here</h1>
+
+const navigate = useNavigate()
+
+async function handleRegister(formData){
+  await axios.post('/api/register', formData)
+  console.log(formData)
+  navigate('/login')
+}
+
+  const fields = {
+    firstName: 'text',
+    lastName: 'text',
+    email: 'email',
+    password: 'password',
+    location: 'text',
+    age: 'number',
+    passwordConfirmation: 'password'
+  }
+
+  return (
+    <div className="form-page">
+      <h1>Register</h1>
+      <Form request={handleRegister} fields={fields} submit="Register" />
+    </div>
+  )
 }
---
+++ client/src/components/subcomponents/Form.jsx
@@ -0,0 +1,70 @@
+import { Fragment, useEffect, useState } from 'react'
+
+export default function Form({ submit, fields, request, onLoad }) {
+
+  const fieldsReduced = Object.fromEntries(
+    Object.entries(fields).map(([key, value]) => [key, value === 'multi' ? [] : ''])
+  );
+
+  // ! State
+  const [formData, setFormData] = useState(fieldsReduced)
+  const [error, setError] = useState('')
+
+  // ! Event driven functions
+  async function handleSubmit(e) {
+    // prevents default form submit behaviour
+    e.preventDefault()
+    try {
+      await request(formData)
+    } catch (error) {
+      console.log(error)
+      console.log(error.response.data)
+      setError(error.response.data)
+    }
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+    setError('')
+  }
+
+  // ! Effects
+  useEffect(() => {
+    async function fillFields() {
+      try {
+        const { data } = await onLoad()
+        setFormData(data)
+      } catch (error) {
+        console.log(error)
+        setError(error.response.data)
+      }
+    }
+    if (onLoad) {
+      fillFields()
+    }
+  }, [onLoad])
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {Object.entries(fields).map(([fieldName, fieldType]) => {
+        return (
+          <Fragment key={fieldName}>
+            <label htmlFor={fieldName}>{fieldName}</label>
+            <input
+              type={fieldType}
+              id={fieldName}
+              name={fieldName}
+              value={formData[fieldName] || ''}
+              onChange={handleChange}
+              placeholder={fieldName}
+            />
+          </Fragment>
+        )
+      })}
+      <button type="submit">{submit}</button>
+    </form>
+  )
+}
+
+
